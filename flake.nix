b2f846718201806137c719fe36b560719053a0b2
desiirenav <desiirenav@hotmail.com>
2025-02-19 23:16:17 -0500

    organizing files

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@ outputs = { self, nixpkgs }@inputs:
     nixosConfigurations = {
       nav = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./configuration.nix];
+        modules = [ ./config/nav.nix];
       };
     };
   };
