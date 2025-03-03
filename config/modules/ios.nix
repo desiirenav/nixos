28371d36311be6dc7e205f1138d4021fcd395f26
desiirenav <desiirenav@hotmail.com>
2025-03-02 23:51:25 -0500

    Updating the ios.nix

--- config/modules/ios.nix
+++ config/modules/ios.nix
@@ -1,9 +1,14 @@
 {config,lib,pkgs,pkgs-unstable,inputs,...}:
 {
-  services.usbmuxd.enable = true;
+  services.usbmuxd = {
+    enable = true;
+    package = pkgs.usbmuxd2;
+  };
 
   environment.systemPackages = with pkgs; [
     libimobiledevice
-    ifuse # optional, to mount using 'ifuse'
+    ifuse
   ];
+
+  services.udev.packages = [ pkgs.libimobiledevice ];
 }
--- config/narayan.nix
+++ config/narayan.nix
@@ -5,7 +5,7 @@
     isNormalUser = true;
     shell = pkgs.fish; 
     description = "Narayan";
-    extraGroups = [ "networkmanager" "wheel" "gamemode" ];
+    extraGroups = [ "networkmanager" "wheel" "gamemode" "fuse" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhgcC+53QIoD9wmc0ziGtCfyivh7lDb65E5ADCP5Bcv narayan@nav"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGugIlZZEY4ywi1P0X7YhNZyZt2ZpTZt12QrGv16pfRh narayan@nixos"
