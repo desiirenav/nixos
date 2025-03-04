49441bbc86d472dea84bb79885e774833443f32b
desiirenav <desiirenav@hotmail.com>
2025-03-03 23:48:42 -0500

    Trying to setup rofi declaratively

---
+++ config/modules/rofi.nix
@@ -0,0 +1,19 @@
+{ lib, config, pkgs, pkgs-unstable, ... }:
+
+{
+  programs.rofi = {
+    enable = true;
+    theme = "default";
+    terminal = "${pkgs.kitty}/bin/kitty";
+    
+    extraConfig = {
+      modi = "drun,run,window";
+      show-icons = true;
+      display-drun = "Applications";
+      display-run = "Run Command";
+      display-window = "Windows";
+      width = 60;
+      lines = 10;
+    };
+  };
+}
